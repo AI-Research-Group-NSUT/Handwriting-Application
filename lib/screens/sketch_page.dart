acf394e49fab9a448c59763871ce9bf9298e030b
Priyanshu Sharma <inbox.priyanshu@gmail.com>
2022-06-29 23:35:32 +0530

    undo, reset and path generation added

--- lib/screens/sketch_page.dart
+++ lib/screens/sketch_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:handwriting/line_param.dart';
 import 'package:handwriting/sketch.dart';
 
@@ -33,6 +34,74 @@ class _SketchPageState extends State<SketchPage> {
           lines: _lines,
           addNewLine: _addNewLine,
           addToCurrentLine: _addToCurrentLine,
+        ),
+        Positioned(
+          bottom: 16,
+          left: 16,
+          child: Container(
+            color: Colors.blue,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      setState(() {
+                        // add lines to undo stack
+
+                        _lines.clear();
+                      });
+                    },
+                    child: const Icon(
+                      Icons.refresh,
+                      color: Colors.white,
+                    ),
+                  ),
+                  const SizedBox(width: 16),
+                  TextButton(
+                    child: const Icon(
+                      Icons.undo,
+                      color: Colors.white,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _lines.removeLast();
+
+                        // add lines to undo stack
+                      });
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        Positioned(
+          bottom: 16,
+          right: 16,
+          child: TextButton(
+            child: const Text(
+              'Copy Path of Lines',
+              style: TextStyle(fontSize: 20, color: Colors.blue),
+            ),
+            onPressed: () {
+              // get path of lines into one array and two array separate by -1 -1
+              final path = _lines.map((line) => line.path).toList();
+
+              List<List<double>> updatedPath = [];
+
+              for (int i = 0; i < path.length; ++i) {
+                for (int j = 0; j < path[i].length; ++j) {
+                  final addendum = [path[i][j].dx, path[i][j].dy];
+                  updatedPath.add(addendum);
+                }
+                updatedPath.add([-1, -1]);
+              }
+
+              // copy path to clipboard
+              Clipboard.setData(ClipboardData(text: updatedPath.toString()));
+            },
+          ),
         ),
       ],
     );
